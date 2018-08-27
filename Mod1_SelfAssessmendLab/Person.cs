80cab381aae7cdbccd9fa8ff2ae73450cac722b3
Martin <mortnmx@hotmail.com>
2018-08-27 22:03:29 +0200

    intiCommit

---
+++ Mod1_SelfAssessmendLab/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPcsCourse_edX_ClassDemo
+{
+    abstract class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _birthYear;
+
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public int BirthYear { get => _birthYear; set => _birthYear = value; }
+
+
+    }
+}
